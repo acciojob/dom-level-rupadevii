63c1f7e512d39b9bab37227a8452cc4e9b10e6b6
rupadevii <rupadevi7094@gmail.com>
2025-10-26 14:16:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function getLevel(element){
+	if(!element) return -1;
+
+	let depth = 1;
+
+	let currentEl = element;
+
+	while(currentEl && currentEl !== document.documentElement){
+		currentEl = currentEl.parentElement;
+		depth += 1;
+	}
+
+	return depth;
+}
+
+const targetEl = document.getElementById('level');
+alert(`Level: ${getLevel(targetEl)}`)
+	
